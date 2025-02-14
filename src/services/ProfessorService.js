dbd10ca2cecb3ed4c95cf7a9d242394af934b366
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-02-14 18:20:24 -0300

    feat: Criando a classe de services para Professores

---
+++ src/services/ProfessorService.js
@@ -0,0 +1,11 @@
+const Services = require("./Services.js");
+
+class ProfessorService extends Services {
+
+    constructor() {
+        super("Professor");
+    }
+ 
+}
+
+module.exports = ProfessorService;
